8016e465b48f1e4704383733a7b73e0161f36c91
Nikhil Kalidasu <skdc13129@gmail.com>
2024-04-20 01:41:08 -0500

    thing

--- runsim.sh
+++ runsim.sh
@@ -7,4 +7,4 @@ filename="008.espresso.din"
 outputname="sample.out"
 
 cd d4-7
-./dineroIV -l1-isize 256K -l1-dsize 256K -l1-irepl r -l1-drepl r -l2-usize 2048K -l2-uassoc 4 -l2-urepl r -l1-ibsize 64 -l1-dbsize 64 -l2-ubsize 64  -informat d -stat-interval 1000 < "../Traces/Spec_Benchmark/$filename" > "$outputname"
+./dineroIV -l1-isize 256K -l1-dsize 256K -l1-irepl r -l1-drepl r -l2-usize 2048K -l2-uassoc 4 -l2-urepl r -l1-ibsize 64 -l1-dbsize 64 -l2-ubsize 64  -informat d < "../Traces/Spec_Benchmark/$filename" > "$outputname"
