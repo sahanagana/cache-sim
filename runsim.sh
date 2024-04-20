cc41357a4f53c33ebce1340f624d6213b57a806c
sahanagana <sahaana.ganapathy@gmail.com>
2024-04-19 21:54:52 -0500

    added trace files, runsim to auto run dinero

---
+++ runsim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filename="$1"
+outputname="$2"
+
+cd d4-7
+./dineroIV -l1-isize 256K -l1-dsize 256K -l1-irepl r -l1-drepl r -l2-usize 2048K -l2-uassoc 4 -l2-urepl r -l1-ibsize 64 -l1-dbsize 64 -l2-ubsize 64  -informat d < "../Traces/Spec_Benchmark/$filename" > "$outputname"
